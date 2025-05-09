1c2cca87403c5d49f794295ee176add5e70e4765
vwkd <33468089+vwkd@users.noreply.github.com>
2025-05-10 00:25:51 +0200

    add tests

--- lib/combinators/iteration/many.test.ts
+++ lib/combinators/iteration/many.test.ts
@@ -1,23 +1,87 @@
 import { assertEquals } from "@std/assert";
-import { digit, many0 } from "../../main.ts";
+import { digit, many } from "../../main.ts";
+import { parseErrors } from "../../../src/errors.ts";
 
-Deno.test("many", () => {
-  assertEquals(many0(digit).parse("23 and more"), {
-    success: true,
-    results: [{
-      value: [2, 3],
-      remaining: " and more",
-      position: { line: 1, column: 2 },
-    }],
+Deno.test("negative arguments", () => {
+  assertEquals(many(digit, -3, -2).parse("abcdef"), {
+    success: false,
+    message: "many: min cannot be negative",
+    position: { line: 1, column: 0 },
   });
+});
 
-  // Matches 0 or more times
-  assertEquals(many0(digit).parse("a"), {
+Deno.test("argument order", () => {
+  assertEquals(many(digit, 3, 2).parse("abcdef"), {
+    success: false,
+    message: "many: max cannot be less than min",
+    position: { line: 1, column: 0 },
+  });
+});
+
+Deno.test("zero arguments", () => {
+  assertEquals(many(digit, 0, 0).parse("abcdef"), {
     success: true,
     results: [{
       value: [],
-      remaining: "a",
+      remaining: "abcdef",
       position: { line: 1, column: 0 },
     }],
+  });
+});
+
+Deno.test("1234ab", () => {
+  assertEquals(many(digit, 2, 3).parse("1234ab"), {
+    success: true,
+    results: [{
+      value: [1, 2, 3],
+      remaining: "4ab",
+      position: { line: 1, column: 3 },
+    }],
+  });
+});
+
+Deno.test("123abc", () => {
+  assertEquals(many(digit, 2, 3).parse("123abc"), {
+    success: true,
+    results: [{
+      value: [1, 2, 3],
+      remaining: "abc",
+      position: { line: 1, column: 3 },
+    }],
+  });
+});
+
+Deno.test("12abcd", () => {
+  assertEquals(many(digit, 2, 3).parse("12abcd"), {
+    success: true,
+    results: [{
+      value: [1, 2],
+      remaining: "abcd",
+      position: { line: 1, column: 2 },
+    }],
+  });
+});
+
+Deno.test("1abcde", () => {
+  assertEquals(many(digit, 2, 3).parse("1abcde"), {
+    success: false,
+    message: parseErrors.digit,
+    position: { line: 1, column: 1 },
+  });
+});
+
+Deno.test("abcdef", () => {
+  assertEquals(many(digit, 2, 3).parse("abcdef"), {
+    success: false,
+    message: parseErrors.digit,
+    position: { line: 1, column: 0 },
+  });
+});
+
+Deno.test("empty string", () => {
+  assertEquals(many(digit, 2, 3).parse(""), {
+    success: false,
+    message: parseErrors.digit,
+    position: { line: 1, column: 0 },
   });
 });
--- lib/combinators/iteration/many0.test.ts
+++ lib/combinators/iteration/many0.test.ts
@@ -1,9 +1,8 @@
 import { assertEquals } from "@std/assert";
-import { digit, many1 } from "../../main.ts";
-import { parseErrors } from "../../../src/errors.ts";
+import { digit, many0 } from "../../main.ts";
 
 Deno.test("123abc", () => {
-  assertEquals(many1(digit).parse("123abc"), {
+  assertEquals(many0(digit).parse("123abc"), {
     success: true,
     results: [{
       value: [1, 2, 3],
@@ -14,7 +13,7 @@ Deno.test("123abc", () => {
 });
 
 Deno.test("12abcd", () => {
-  assertEquals(many1(digit).parse("12abcd"), {
+  assertEquals(many0(digit).parse("12abcd"), {
     success: true,
     results: [{
       value: [1, 2],
@@ -25,7 +24,7 @@ Deno.test("12abcd", () => {
 });
 
 Deno.test("1abcde", () => {
-  assertEquals(many1(digit).parse("1abcde"), {
+  assertEquals(many0(digit).parse("1abcde"), {
     success: true,
     results: [{
       value: [1],
@@ -36,17 +35,23 @@ Deno.test("1abcde", () => {
 });
 
 Deno.test("abcdef", () => {
-  assertEquals(many1(digit).parse("abcdef"), {
-    success: false,
-    message: parseErrors.digit,
-    position: { line: 1, column: 0 },
+  assertEquals(many0(digit).parse("abcdef"), {
+    success: true,
+    results: [{
+      value: [],
+      remaining: "abcdef",
+      position: { line: 1, column: 0 },
+    }],
   });
 });
 
 Deno.test("empty string", () => {
-  assertEquals(many1(digit).parse(""), {
-    success: false,
-    message: parseErrors.digit,
-    position: { line: 1, column: 0 },
+  assertEquals(many0(digit).parse(""), {
+    success: true,
+    results: [{
+      value: [],
+      remaining: "",
+      position: { line: 1, column: 0 },
+    }],
   });
 });
