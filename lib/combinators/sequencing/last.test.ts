60a187d9aae9414efed17a9c415ad07451cf13f3
vwkd <33468089+vwkd@users.noreply.github.com>
2025-05-11 06:27:03 +0200

    add tests

--- lib/combinators/sequencing/last.test.ts
+++ lib/combinators/sequencing/last.test.ts
@@ -1,24 +1,60 @@
 import { assertEquals } from "@std/assert";
-import { digit, last, letter } from "../../main.ts";
+import { digit, last, letter, whitespace } from "../../main.ts";
 import { parseErrors } from "../../../src/errors.ts";
 
-Deno.test("last", () => {
-  assertEquals(last(digit, letter).parse("1a"), {
+Deno.test("1a 2b ", () => {
+  assertEquals(last(digit, letter, whitespace).parse("1a 2b "), {
     success: true,
     results: [{
-      value: "a",
-      remaining: "",
-      position: { line: 1, column: 2 },
+      value: " ",
+      remaining: "2b ",
+      position: { line: 1, column: 3 },
     }],
   });
+});
 
-  assertEquals(last(digit, letter).parse("12"), {
+Deno.test("1abcde", () => {
+  assertEquals(last(digit, letter, whitespace).parse("1abcde"), {
+    success: false,
+    message: "Expected a white space character",
+    position: { line: 1, column: 2 },
+  });
+});
+
+Deno.test("1a", () => {
+  assertEquals(last(digit, letter, whitespace).parse("1a"), {
+    success: false,
+    message: "Expected a white space character",
+    position: { line: 1, column: 2 },
+  });
+});
+
+Deno.test("12abcd", () => {
+  assertEquals(last(digit, letter, whitespace).parse("12abcd"), {
     success: false,
     message: parseErrors.letter,
     position: { line: 1, column: 1 },
   });
+});
 
-  assertEquals(last(digit, letter).parse("ab"), {
+Deno.test("1", () => {
+  assertEquals(last(digit, letter, whitespace).parse("1"), {
+    success: false,
+    message: parseErrors.letter,
+    position: { line: 1, column: 1 },
+  });
+});
+
+Deno.test("abcdef", () => {
+  assertEquals(last(digit, letter, whitespace).parse("abcdef"), {
+    success: false,
+    message: parseErrors.digit,
+    position: { line: 1, column: 0 },
+  });
+});
+
+Deno.test("empty string", () => {
+  assertEquals(last(digit, letter, whitespace).parse(""), {
     success: false,
     message: parseErrors.digit,
     position: { line: 1, column: 0 },
