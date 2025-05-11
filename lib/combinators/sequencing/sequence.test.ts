0d16626686f24b4534a12a31a54696d734931065
vwkd <33468089+vwkd@users.noreply.github.com>
2025-05-11 11:55:47 +0200

    add tests

--- lib/combinators/sequencing/sequence.test.ts
+++ lib/combinators/sequencing/sequence.test.ts
@@ -1,18 +1,101 @@
 import { assertEquals } from "@std/assert";
-import { digit, literal, result, sequence } from "../../main.ts";
+import { digit, letter, sequence } from "../../main.ts";
+import { parseErrors } from "../../../src/errors.ts";
 
-Deno.test("sequence", () => {
-  assertEquals(
-    sequence(literal("a"), digit).bind(([str, num]) =>
-      result(str.toUpperCase() + `${num * 100}`)
-    ).parse("a3"),
-    {
-      success: true,
-      results: [{
-        value: "A300",
-        remaining: "",
-        position: { line: 1, column: 2 },
-      }],
-    },
-  );
+Deno.test("1a2b", () => {
+  assertEquals(sequence(digit, letter, digit).parse("1a2b"), {
+    success: true,
+    results: [{
+      value: [1, "a", 2],
+      remaining: "b",
+      position: { line: 1, column: 3 },
+    }],
+  });
+});
+
+Deno.test("1a2", () => {
+  assertEquals(sequence(digit, letter, digit).parse("1a2"), {
+    success: true,
+    results: [{
+      value: [1, "a", 2],
+      remaining: "",
+      position: { line: 1, column: 3 },
+    }],
+  });
+});
+
+Deno.test("1a", () => {
+  assertEquals(sequence(digit, letter, digit).parse("1a"), {
+    success: false,
+    message: parseErrors.digit,
+    position: { line: 1, column: 2 },
+  });
+});
+
+Deno.test("1", () => {
+  assertEquals(sequence(digit, letter, digit).parse("1"), {
+    success: false,
+    message: parseErrors.letter,
+    position: { line: 1, column: 1 },
+  });
+});
+
+Deno.test("empty string", () => {
+  assertEquals(sequence(digit, letter, digit).parse(""), {
+    success: false,
+    message: parseErrors.digit,
+    position: { line: 1, column: 0 },
+  });
+});
+
+Deno.test("1a", () => {
+  assertEquals(sequence(digit).parse("1a"), {
+    success: true,
+    results: [{
+      value: [1],
+      remaining: "a",
+      position: { line: 1, column: 1 },
+    }],
+  });
+});
+
+Deno.test("1", () => {
+  assertEquals(sequence(digit).parse("1"), {
+    success: true,
+    results: [{
+      value: [1],
+      remaining: "",
+      position: { line: 1, column: 1 },
+    }],
+  });
+});
+
+Deno.test("empty string", () => {
+  assertEquals(sequence(digit).parse(""), {
+    success: false,
+    message: parseErrors.digit,
+    position: { line: 1, column: 0 },
+  });
+});
+
+Deno.test("abc", () => {
+  assertEquals(sequence().parse("abc"), {
+    success: true,
+    results: [{
+      value: [],
+      remaining: "abc",
+      position: { line: 1, column: 0 },
+    }],
+  });
+});
+
+Deno.test("empty string", () => {
+  assertEquals(sequence().parse(""), {
+    success: true,
+    results: [{
+      value: [],
+      remaining: "",
+      position: { line: 1, column: 0 },
+    }],
+  });
 });
