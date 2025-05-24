887f712f5347306d06940cec03ed5737041bbadc
vwkd <33468089+vwkd@users.noreply.github.com>
2025-05-24 03:00:49 +0200

    add tests

--- src/combinators/iteration/many0.test.ts
+++ src/combinators/iteration/many0.test.ts
@@ -1,10 +1,9 @@
 import { assertEquals } from "@std/assert";
-import { many1 } from "$combinators";
+import { many0 } from "$combinators";
 import { digit } from "$common";
-import { parseErrors } from "../../errors.ts";
 
 Deno.test("123abc", () => {
-  assertEquals(many1(digit).parse("123abc"), {
+  assertEquals(many0(digit).parse("123abc"), {
     success: true,
     results: [{
       value: [1, 2, 3],
@@ -15,7 +14,7 @@ Deno.test("123abc", () => {
 });
 
 Deno.test("12abcd", () => {
-  assertEquals(many1(digit).parse("12abcd"), {
+  assertEquals(many0(digit).parse("12abcd"), {
     success: true,
     results: [{
       value: [1, 2],
@@ -26,7 +25,7 @@ Deno.test("12abcd", () => {
 });
 
 Deno.test("1abcde", () => {
-  assertEquals(many1(digit).parse("1abcde"), {
+  assertEquals(many0(digit).parse("1abcde"), {
     success: true,
     results: [{
       value: [1],
@@ -37,17 +36,23 @@ Deno.test("1abcde", () => {
 });
 
 Deno.test("abcdef", () => {
-  assertEquals(many1(digit).parse("abcdef"), {
-    success: false,
-    message: parseErrors.digit,
-    position: { line: 1, column: 0 },
+  assertEquals(many0(digit).parse("abcdef"), {
+    success: true,
+    results: [{
+      value: [],
+      remaining: "abcdef",
+      position: { line: 1, column: 0 },
+    }],
   });
 });
 
 Deno.test("empty string", () => {
-  assertEquals(many1(digit).parse(""), {
-    success: false,
-    message: parseErrors.digit,
-    position: { line: 1, column: 0 },
+  assertEquals(many0(digit).parse(""), {
+    success: true,
+    results: [{
+      value: [],
+      remaining: "",
+      position: { line: 1, column: 0 },
+    }],
   });
 });
---
+++ src/combinators/iteration/sepBy.test.ts
@@ -0,0 +1,104 @@
+import { assertEquals } from "@std/assert";
+import { sepBy } from "$combinators";
+import { digit, literal } from "$common";
+import { parseErrors } from "../../errors.ts";
+
+Deno.test("negative arguments", () => {
+  assertEquals(sepBy(digit, literal(","), -3, -2).parse("1,2,3,4,5,6"), {
+    success: false,
+    message: "sepBy: min cannot be negative",
+    position: { line: 1, column: 0 },
+  });
+});
+
+Deno.test("argument order", () => {
+  assertEquals(sepBy(digit, literal(","), 3, 2).parse("1,2,3,4,5,6"), {
+    success: false,
+    message: "sepBy: max cannot be less than min",
+    position: { line: 1, column: 0 },
+  });
+});
+
+Deno.test("zero arguments", () => {
+  assertEquals(sepBy(digit, literal(","), 0, 0).parse("1,2,3,4,5,6"), {
+    success: true,
+    results: [{
+      value: [],
+      remaining: "1,2,3,4,5,6",
+      position: { line: 1, column: 0 },
+    }],
+  });
+});
+
+Deno.test("1,2,3,4,a,b", () => {
+  assertEquals(sepBy(digit, literal(","), 2, 3).parse("1,2,3,4,a,b"), {
+    success: true,
+    results: [{
+      value: [1, 2, 3],
+      remaining: ",4,a,b",
+      position: { line: 1, column: 5 },
+    }],
+  });
+});
+
+Deno.test("1,2,3,a,b,c", () => {
+  assertEquals(sepBy(digit, literal(","), 2, 3).parse("1,2,3,a,b,c"), {
+    success: true,
+    results: [{
+      value: [1, 2, 3],
+      remaining: ",a,b,c",
+      position: { line: 1, column: 5 },
+    }],
+  });
+});
+
+Deno.test("1,2,a,b,c,d", () => {
+  assertEquals(sepBy(digit, literal(","), 2, 3).parse("1,2,a,b,c,d"), {
+    success: true,
+    results: [{
+      value: [1, 2],
+      remaining: ",a,b,c,d",
+      position: { line: 1, column: 3 },
+    }],
+  });
+});
+
+Deno.test("1,a,b,c,d,e", () => {
+  assertEquals(sepBy(digit, literal(","), 2, 3).parse("1,a,b,c,d,e"), {
+    success: false,
+    message: parseErrors.digit,
+    position: { line: 1, column: 2 },
+  });
+});
+
+Deno.test("1abcde", () => {
+  assertEquals(sepBy(digit, literal(","), 2, 3).parse("1abcde"), {
+    success: false,
+    message: "Expected ',', but got 'a'",
+    position: { line: 1, column: 1 },
+  });
+});
+
+Deno.test("abcdef", () => {
+  assertEquals(sepBy(digit, literal(","), 2, 3).parse("abcdef"), {
+    success: false,
+    message: parseErrors.digit,
+    position: { line: 1, column: 0 },
+  });
+});
+
+Deno.test(",", () => {
+  assertEquals(sepBy(digit, literal(","), 2, 3).parse(","), {
+    success: false,
+    message: parseErrors.digit,
+    position: { line: 1, column: 0 },
+  });
+});
+
+Deno.test("empty string", () => {
+  assertEquals(sepBy(digit, literal(","), 2, 3).parse(""), {
+    success: false,
+    message: parseErrors.digit,
+    position: { line: 1, column: 0 },
+  });
+});
